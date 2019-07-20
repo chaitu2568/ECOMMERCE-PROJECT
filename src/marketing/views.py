b6c0c8c0ee3f42e18e18bec79ddf908d10ded7a7
Krishna Chaitanya Bogavalli <krishnachaitanyabogavalli@Krishnas-MacBook-Pro.local>
2019-07-20 16:18:24 -0700

    MAIL-CHIMP WEBHOOK

--- src/marketing/views.py
+++ src/marketing/views.py
@@ -4,7 +4,12 @@ from django.views.generic import UpdateView
 from django.shortcuts import render, redirect
 from .forms import MarketingPreferenceForm
 from .models import MarketingPreference
+from django.conf import settings
+from .utils import Mailchimp
+from .mixins import CsrfExemptMixin
+from django.views.generic import UpdateView, View
 
+MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
 
 class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
     form_class = MarketingPreferenceForm
@@ -29,3 +34,55 @@ class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
         user = self.request.user
         obj, created = MarketingPreference.objects.get_or_create(user=user) # get_absolute_url
         return obj
+
+# Method:POST
+# "root":
+# "type": "subscribe"
+# "fired_at": "2019-07-20 22:34:26"
+# "data":
+# "id": "97ec415b05"
+# "email": "kbogaval@gmail.com"
+# "email_type": "html"
+# "ip_opt": "72.201.13.9"
+# "web_id": "347480181"
+# "merges":
+# "EMAIL": "kbogaval@gmail.com"
+# "FNAME": ""
+# "LNAME": ""
+# "ADDRESS": ""
+# "PHONE": ""
+# "BIRTHDAY": ""
+# "list_id": "b5113c5eb4"
+# host: en37vuw88zr6w.x.pipedream.net
+# Content-Type: application/x-www-form-urlencoded
+# User-Agent: MailChimp
+# Content-Length: 418
+# Connection: keep-alive
+
+class MailchimpWebhookView(CsrfExemptMixin, View):
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponse("Thank you", status=200)
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        list_id = data.get('data[list_id]')
+        if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
+            hook_type = data.get("roottype]")
+            email = data.get('data[email]')
+            response_status, response = Mailchimp().check_subcription_status(email)
+            sub_status  = response['status']
+            is_subbed = None
+            mailchimp_subbed = None
+            if sub_status == "subscribed":
+                is_subbed, mailchimp_subbed  = (True, True)
+            elif sub_status == "unsubscribed":
+                is_subbed, mailchimp_subbed  = (False, False)
+            if is_subbed is not None and mailchimp_subbed is not None:
+                qs = MarketingPreference.objects.filter(user__email__iexact=email)
+                if qs.exists():
+                    qs.update(
+                            subscribed=is_subbed,
+                            mailchimp_subscribed=mailchimp_subbed,
+                            mailchimp_msg=str(data))
+        return HttpResponse("Thank you", status=200)
+
+
